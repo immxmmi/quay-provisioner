a9457257e0d5702eec867440afe92539f475a9dc
immxmmi <immxmmi@gmx.at>
2025-12-02 12:04:11 +0100

    Refactor main.py to use Config for pipeline file path resolution

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from engine.pipeline_engine import PipelineEngine
-from config.loader import TARGET_FILE
+from config.loader import Config
 
+config = Config()
 engine = PipelineEngine()
 
-pipeline = engine.load_pipeline(TARGET_FILE)
+pipeline = engine.load_pipeline(config.pipeline_file)
 engine.debug_print(pipeline)
 engine.run_pipeline(pipeline)
